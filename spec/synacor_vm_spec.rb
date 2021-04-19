0fadca065a5651bd100fadcbd9a341bdf81bb8ee
Philip Corliss <pcorliss@bigdevbox.com>
2021-04-19 21:57:23 +0000

    hint input handled

--- spec/synacor_vm_spec.rb
+++ spec/synacor_vm_spec.rb
@@ -1,7 +1,98 @@
 require_relative '../synacor_vm.rb'
 
 describe SynacorVm do
-  it 'asserts true is true' do
-    expect(true).to be_truthy
+  let(:input) { "9,32768,32769,4,19,32768" }
+  let(:vm) { SynacorVm.new(input) }
+
+  describe '#new' do
+    it "inits eight registers" do
+      expect(vm.registers.count).to eq(8)
+      expect(vm.registers[0]).to eq(0)
+    end
+
+    it "inits a program" do
+      expect(vm.program.count).to eq(6)
+      expect(vm.program[0]).to eq(9)
+    end
+
+    it "inits a position" do
+      expect(vm.pos).to eq(0)
+    end
+  end
+
+  describe '#run' do
+    it 'runs until halt is encountered' do
+      expect { vm.run }.to output.to_stdout
+      expect(vm.halt).to be_truthy
+    end
+
+    context "validation" do
+      it 'handles the basic input' do
+        vm.registers[1] = 60
+        expect { vm.run }.to output('@').to_stdout
+        expect(vm.registers[0]).to eq(64)
+      end
+    end
+  end
+
+  describe '#get_arg' do
+    it 'gets an argument from the program' do
+      expect(vm.get_arg).to eq([9, 9])
+    end
+
+    it 'returns the original as well as the val' do
+      vm.get_arg
+      vm.registers[0] = 99
+      expect(vm.get_arg).to eq([32768, 99])
+    end
+
+    it 'increments the pos counter' do
+      vm.get_arg
+      expect(vm.pos).to eq(1)
+    end
+  end
+
+  describe '#step' do
+    describe '#halt' do
+      it 'sets halt to true' do
+        expect(vm.halt).to be_falsey
+        vm.step(0)
+        expect(vm.halt).to be_truthy
+      end
+    end
+
+    describe '#out' do
+      it "writes ascii char to STDOUT" do
+        vm.program.unshift(64)
+        expect { vm.step(19) }.to output('@').to_stdout
+      end
+    end
+
+    describe '#noop' do
+      # xit "does nothing" do
+      # end
+    end
+
+    describe '#add' do
+      it "adds <b> and <c> and assigns to a" do
+        vm.program.unshift(32768, 1, 2)
+        vm.step(9)
+        expect(vm.registers[0]).to eq(3)
+      end
+
+      it "performs math modulo 32768" do
+        vm.program.unshift(32768, 32765, 32765)
+        vm.step(9)
+        expect(vm.registers[0]).to eq(32762)
+      end
+
+      it "reads from registers instead of using literals" do
+        vm.registers[0] = 1
+        vm.registers[1] = 2
+        vm.program.unshift(32768, 32768, 32769)
+        vm.step(9)
+        expect(vm.registers[0]).to eq(3)
+      end
+    end
   end
 end
--- synacor_vm.rb
+++ synacor_vm.rb
@@ -1,4 +1,44 @@
 class SynacorVm
+  attr_reader :registers, :program, :halt, :pos
 
+  def initialize(input = '')
+    @registers = 8.times.map { 0 }
+    @program = input.split(',').map(&:to_i)
+    @halt = false
+    @pos = 0
+  end
 
+  def get_arg
+    a = @program[@pos]
+    a_val = a && a >= 32768 ? @registers[a - 32768] : a
+    @pos += 1
+    [a, a_val]
+  end
+
+  def step(op)
+    case op
+    when 0 # halt
+      @halt = true
+    when 9 # add
+      a, _ = get_arg
+      _, b_val = get_arg
+      _, c_val = get_arg
+      @registers[a - 32768] = (b_val + c_val) % 32768
+    when 19 # out
+      _, a_val = get_arg
+      print a_val.chr
+    when 21 #noop
+    else
+      puts "Unhandled OpCode encountered: ${op}"
+      @halt = true
+    end
+  end
+
+  def run
+    while !halt do
+      op, _ = get_arg
+      op ||= 0
+      step(op)
+    end
+  end
 end
